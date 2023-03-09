49df795f10e316fcfb6d2c3f07a115d76cd4b1b2
HyoYoungJung <hy8grace@gmail.com>
2023-03-09 11:38:41 +0900

    module1

---
+++ module1/src/programmer/App.java
@@ -0,0 +1,12 @@
+package programmer;
+
+//import developer.Secret;
+import utils.Open;
+
+public class App {
+
+	public static void main(String[] args) {
+		new Open().yahoo();
+//		new Secret().hush();
+	}
+}
